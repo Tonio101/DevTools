fb329186c1056528e1ee9d3bfcd24e0111458a6e
Antonio <antonio@antonio101.com>
2020-05-24 16:48:26 -0400

    Change Tmux install path

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script for installing tmux on systems where you don't have root access.
-# tmux will be installed in $HOME/local/bin.
+# tmux will be installed in $TMUX_BUILD_DIR/bin.
 # It's assumed that wget and a C/C++ compiler are installed.
 # gcc, g++, make
 
@@ -9,11 +9,12 @@
 set -e
 
 TMUX_VERSION=3.0a
+TMUX_BUILD_DIR=$HOME/local/TMUX
 
 # create our directories
-if [ ! -d "$HOME/local" ]; then
-    echo "Creating a local directory in $HOME"
-    mkdir -p $HOME/local
+if [ ! -d "$TMUX_BUILD_DIR" ]; then
+    echo "Creating a local directory in $TMUX_BUILD_DIR"
+    mkdir -p $TMUX_BUILD_DIR
 fi
 mkdir -p $HOME/tmux_tmp
 cd $HOME/tmux_tmp
@@ -29,7 +30,7 @@ wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz
 ############
 tar -xvzf libevent-2.1.8-stable.tar.gz
 cd libevent-2.1.8-stable
-./configure --prefix=$HOME/local --disable-shared
+./configure --prefix=$TMUX_BUILD_DIR --disable-shared
 make
 make install
 cd ..
@@ -40,7 +41,7 @@ cd ..
 tar -xvzf ncurses-6.0.tar.gz
 cd ncurses-6.0
 sed -i 's/preprocessor=\"$1 -DNCURSES_INTERNALS -I..\/include\"/preprocessor=\"$1 -P -DNCURSES_INTERNALS -I..\/include\"/g' ./ncurses/base/MKlib_gen.sh
-./configure --prefix=$HOME/local
+./configure --prefix=$TMUX_BUILD_DIR
 make
 make install
 cd ..
@@ -50,12 +51,12 @@ cd ..
 ############
 tar -xvzf tmux-${TMUX_VERSION}.tar.gz
 cd tmux-${TMUX_VERSION}
-./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
-CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
-cp tmux $HOME/local/bin
+./configure CFLAGS="-I$TMUX_BUILD_DIR/include -I$TMUX_BUILD_DIR/include/ncurses" LDFLAGS="-L$TMUX_BUILD_DIR/lib -L$TMUX_BUILD_DIR/include/ncurses -L$TMUX_BUILD_DIR/include"
+CPPFLAGS="-I$TMUX_BUILD_DIR/include -I$TMUX_BUILD_DIR/include/ncurses" LDFLAGS="-static -L$TMUX_BUILD_DIR/include -L$TMUX_BUILD_DIR/include/ncurses -L$TMUX_BUILD_DIR/lib" make
+cp tmux $TMUX_BUILD_DIR/bin
 cd ..
 
 # cleanup
 rm -rf $HOME/tmux_tmp
 
-echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
+echo "\$TMUX_BUILD_DIR/bin/tmux is now available. You can optionally add $TMUX_BUILD_DIR/bin to your PATH."
